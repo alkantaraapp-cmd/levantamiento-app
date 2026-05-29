6c4c6d5fc99fa45d9cca8826d3939ad33e60018e
alkantaraapp-cmd <alkantaraapp@gmail.com>
2026-05-29 02:56:33 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
    CONFIGURACIÓN - Pega tu URL de Apps Script aquí
    ============================================================ */
 const CFG = {
-  SCRIPT_URL: 'https://script.google.com/macros/s/TU_DEPLOYMENT_ID/exec',
+  SCRIPT_URL: 'https://script.google.com/macros/s/https://script.google.com/macros/s/AKfycbwaaDJv0YTqt7Pwahc_ocvm_o0uX0c-XvpIRelgxxyTmCzkYTq02ilyy1RUaOKFEV9g/exec',
   APP_NAME: 'Sistema de Levantamiento'
 };
 
