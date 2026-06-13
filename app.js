91636b6c4ac44e82bdcda1c3151efec8509bc02d
alkantaraapp-cmd <alkantaraapp@gmail.com>
2026-06-13 09:28:20 -0400

    Update app.js

--- app.js
+++ app.js
@@ -982,7 +982,7 @@ function agregarNicho() {
         '<input type="text" id="nicho-nombre-' + idx + '" placeholder="Nombre o VACÍO">' +
       '</div>' +
       '<div class="field-group"><label>Fecha de Fallecimiento</label>' +
-        '<input type="text" id="nicho-fecha-' + idx + '" placeholder="DD/MM/AAAA">' +
+        '<input type="text" id="nicho-fecha-' + idx + '" placeholder="dd/mm/aaaa" inputmode="numeric">' +
         '</div>' +
         '<div class="field-group full"><label>Foto del Nicho</label>' +
         '<div class="photo-box">' +
